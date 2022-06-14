5553025859e35e34dc7e454cdabd61dc20a6f485
Pozitronik <antikillerxxl@gmail.com>
2022-06-14 15:47:53 +0300

    Sample test

---
+++ tests/functional/UsersCest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types = 1);
+
+use app\models\Users;
+use Codeception\Exception\ModuleException;
+use yii\base\InvalidConfigException;
+
+/**
+ * Class UsersCest
+ */
+class UsersCest {
+
+	/**
+	 * @param FunctionalTester $I
+	 * @throws Throwable
+	 * @throws ModuleException
+	 * @throws InvalidConfigException
+	 * @throws Exception
+	 */
+	public function create(FunctionalTester $I):void {
+		$user = Users::CreateUser()->saveAndReturn();
+
+		$I->amLoggedInAs($user);
+		$I->amOnRoute('users/create');
+		$I->seeResponseCodeIs(200);
+		$I->submitForm("#users-create", [
+			'Users' => [
+				'username' => 'Test Successful',
+				'login' => 'test_user_2',
+				'password' => '123',
+			]
+		]);
+		$I->seeResponseCodeIs(200);
+		$I->seeInCurrentUrl('users/index');
+		$I->assertCount(2, Users::find()->all());
+		$model = Users::findOne(['username' => 'Test Successful']);
+		$I->assertNotNull($model);
+		$I->assertEquals(2, $model->id);
+		$I->assertEquals('test_user_2', $model->login);
+		$I->assertEquals('123', $model->password);
+	}
+
+	/**
+	 * @param FunctionalTester $I
+	 * @return void
+	 * @throws ModuleException
+	 * @throws Exception
+	 */
+	public function testViewTitles(FunctionalTester $I):void {
+		$user = Users::CreateUser()->saveAndReturn();
+		$I->amLoggedInAs($user);
+		$I->amOnRoute('users/view?id=1');
+		$I->seeResponseCodeIs(200);
+		$I->canSeeInTitle("Просмотр {$user->username}");
+	}
+}
