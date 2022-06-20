4098c26da739f03bc2bd2a0170316919a5115f4a
Pozitronik <antikillerxxl@gmail.com>
2022-06-20 11:22:45 +0300

    Генератор trace_id

--- tests/_support/Helper/Functional.php
+++ tests/_support/Helper/Functional.php
@@ -1,10 +1,24 @@
 <?php
+declare(strict_types = 1);
+
 namespace Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
+use Codeception\Module;
+use Exception;
 
-class Functional extends \Codeception\Module
-{
+/**
+ * Class Functional
+ */
+class Functional extends Module {
+
+	/**
+	 * @return string
+	 * @throws Exception
+	 */
+	public static function gen_trace_header():string {
+		return sprintf("00-%016x-%08x-00", random_int(0, 0xfffffffffffffff), random_int(0, 0xffffffff));
+	}
 
 }
