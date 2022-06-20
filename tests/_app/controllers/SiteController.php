6c8fffd52d3ea94d4bbf5a7ebd83f6a21f61b667
Pozitronik <antikillerxxl@gmail.com>
2022-06-20 11:22:59 +0300

    actionApi для теста

--- tests/_app/controllers/SiteController.php
+++ tests/_app/controllers/SiteController.php
@@ -4,19 +4,43 @@ declare(strict_types = 1);
 namespace app\controllers;
 
 use Yii;
+use yii\filters\ContentNegotiator;
 use yii\helpers\Html;
 use yii\web\Controller;
+use yii\web\Response;
 
 /**
  * class SiteController
  */
 class SiteController extends Controller {
-
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors():array {
+		return [
+			'contentNegotiator' => [
+				'class' => ContentNegotiator::class,
+				'formats' => [
+					'application/json' => Response::FORMAT_JSON
+				],
+				'only' => [
+					'api'
+				]
+			],
+		];
+	}
 	/**
 	 * @return string
 	 */
 	public function actionIndex():string {
 		return "hello";
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function actionApi():array {
+		return ['status' => 'ok'];
 	}
 
 	/**
